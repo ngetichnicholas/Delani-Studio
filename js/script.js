97c708c27ba80d46b4335595aacd82e412a0838c
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-17 13:14:21 +0300

    Add javascript function to alert user when message is submitted

--- js/script.js
+++ js/script.js
@@ -19,3 +19,21 @@ $(document).ready(function() {
     }
   );
 });
+function message() {
+  let userName = document.getElementById("name").value;
+  let userEmail =document.getElementById("email").value;
+  let userMessage =document.getElementById("text-message").value;
+
+  if (userName.lenght === 0 ) {
+    alert("Enter your name before submitting!");
+  }
+  else if (userMessage.lenght === 0) {
+    alert("Enter your email address before submitting!");
+  }
+  else if (userMessage.lenght === 0) {
+    alert("Enter your message or comment before submitting!");
+  }
+  else {
+    alert("Hello " +userName +", we have received your message. Thank you for reaching out to us.")
+  }
+}
