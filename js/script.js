70ae2de80428d3afe3ec3a27c274b866259c0f86
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-17 13:29:32 +0300

    Update form and javascript

--- js/script.js
+++ js/script.js
@@ -26,14 +26,20 @@ function message() {
 
   if (userName.length === 0 ) {
     alert("Enter your name before submitting!");
+    throw new Error;  //Abort javascript execution from here
   }
-  else if (userMessage.length === 0) {
+  else if (userEmail.length === 0) {
     alert("Enter your email address before submitting!");
+    throw new Error;  //Abort javascript execution from here
   }
   else if (userMessage.length === 0) {
     alert("Enter your message or comment before submitting!");
+    throw new Error;  //Abort javascript execution from here
   }
   else {
     alert("Hello " +userName +", we have received your message. Thank you for reaching out to us.")
   }
+  let form = document.getElementsByName("message-form")[0]; 
+  form.reset();  // Clear form fields
+  return false; // Prevent page refresh 
 }
